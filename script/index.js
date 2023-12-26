d5542babef7da4380d5d7c0fcc8b0cfe8dc132a2
IxAntho <isabar735@icloud.com>
2023-12-26 01:26:14 -0500

    Services info update

--- script/index.js
+++ script/index.js
@@ -4,9 +4,10 @@ function scrollToSection(sectionId) {
 
 // Get the navbar element
 const navBar = document.getElementById("navbar");
-const navLinks = document.getElementById("nav__links");
+const navLinks = document.querySelectorAll(".nav__link");
+const logo = document.getElementById("navLogo");
 
-// Get the height of the info bar
+// Get the height of the info bar with .offsetHeight
 const headerSocialHeight =
   document.querySelector(".header__social").offsetHeight;
 
@@ -16,10 +17,42 @@ window.addEventListener("scroll", () => {
   if (window.scrollY > headerSocialHeight) {
     // Add the 'fixed' class to the navbar to make it fixed
     navBar.classList.add("nav__fixed");
-    navLinks.classList.add("nav__links_fixed");
+    logo.src = "../images/FDTSoloLogo.png";
+    logo.classList.add("nav__logo_fixed");
+    navLinks.forEach((link) => {
+      link.style.color = "#009e00";
+    });
   } else {
     // Remove the 'fixed' class to revert to the initial state
     navBar.classList.remove("nav__fixed");
-    navLinks.classList.remove("nav__links_fixed");
+    logo.src = "../images/FdT.png";
+    logo.classList.remove("nav__logo_fixed");
+    navLinks.forEach((link) => {
+      link.style.color = "#cccccc";
+    });
   }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const dynamicTextElement = document.getElementById("dynamic-text");
+  const words = ["Orgánico", "Sustentable", "Alta Calidad", "Agricultura"];
+  let index = 0;
+
+  function updateText() {
+    // Reset the style before changing the content
+    dynamicTextElement.style.animation = "none";
+    dynamicTextElement.offsetHeight; // Trigger reflow to apply style reset
+    dynamicTextElement.style.animation = null;
+
+    // Change the content
+    dynamicTextElement.textContent = words[index];
+
+    // Apply the animation
+    dynamicTextElement.style.animation = "slideUp 0.5s ease";
+
+    index = (index + 1) % words.length;
+  }
+
+  // Update the text every 2 seconds (adjust as needed)
+  setInterval(updateText, 2000);
 });
