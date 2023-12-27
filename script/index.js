f3821b09f99b5b6f7e6f78789096c63bbcc48075
IxAntho <isabar735@icloud.com>
2023-12-26 22:12:44 -0500

    gallery section update

--- script/index.js
+++ script/index.js
@@ -20,7 +20,7 @@ window.addEventListener("scroll", () => {
     logo.src = "../images/FDTFixed.png";
     logo.classList.add("nav__logo_fixed");
     navLinks.forEach((link) => {
-      link.style.color = "#009e00";
+      link.classList.add("nav__link_fixed");
     });
   } else {
     // Remove the 'fixed' class to revert to the initial state
@@ -28,7 +28,7 @@ window.addEventListener("scroll", () => {
     logo.src = "../images/FdT.png";
     logo.classList.remove("nav__logo_fixed");
     navLinks.forEach((link) => {
-      link.style.color = "#cccccc";
+      link.classList.remove("nav__link_fixed");
     });
   }
 });
@@ -64,10 +64,10 @@ document.addEventListener("DOMContentLoaded", function () {
   setInterval(updateText, 2000);
 });
 
-document.addEventListener("DOMContentLoaded", function () {
+/*document.addEventListener("DOMContentLoaded", function () {
   const container = document.querySelector(".gallery__container");
-  const sectionBackground = document.querySelector(".gallery__background");
   const heading = document.querySelector(".gallery__heading");
+  const sectionBackground = document.querySelector(".gallery__background");
   const text = document.querySelector(".gallery__span");
   const imageGallery = document.querySelector(".gallery-wrap");
 
@@ -75,8 +75,8 @@ document.addEventListener("DOMContentLoaded", function () {
     const scrollPosition = window.scrollY;
 
     // Adjust the following values as needed
-    const headingChangePosition = 200;
-    const imageGalleryAppearPosition = 400;
+    const headingChangePosition = 600;
+    const imageGalleryAppearPosition = 600;
 
     if (scrollPosition > headingChangePosition) {
       heading.textContent = "Flor de Tierra";
@@ -92,4 +92,59 @@ document.addEventListener("DOMContentLoaded", function () {
       imageGallery.style.display = "flex";
     }
   });
+});*/
+
+/*document.addEventListener("DOMContentLoaded", function () {
+  const container = document.querySelector(".gallery__container");
+  const background = document.querySelector(".gallery__background");
+  const heading = document.querySelector(".gallery__heading");
+  const span = document.querySelector(".gallery__span");
+  const imageGallery = document.querySelector("#image-gallery");
+
+  window.addEventListener("scroll", function () {
+    const scrollPosition = window.scrollY;
+
+    // Adjust the following values as needed
+    const headingChangePosition = 200;
+    const imageGalleryAppearPosition = 400;
+
+    if (scrollPosition > headingChangePosition) {
+      heading.textContent = "New Heading";
+      span.textContent = "New Text";
+      heading.style.opacity = 0;
+      span.style.opacity = 0;
+      container.style.top = "0";
+      background.style.filter = "blur(10px)";
+      background.style.backgroundColor = "white";
+    }
+
+    if (scrollPosition > imageGalleryAppearPosition) {
+      imageGallery.style.display = "flex";
+    }
+  });
+});*/
+
+// Get references to the necessary elements
+const gallery = document.querySelector(".gallery");
+const galleryItems = document.querySelectorAll(".gallery__item");
+
+// Add event listeners to each gallery item
+galleryItems.forEach((item) => {
+  item.addEventListener("mouseenter", handleItemHover);
+  item.addEventListener("mouseleave", handleItemLeave);
 });
+
+// Function to handle item hover
+function handleItemHover(event) {
+  // Get the background image of the hovered item
+  const backgroundImage = getComputedStyle(event.target).backgroundImage;
+
+  // Set the background of the gallery to the hovered item's background image
+  gallery.style.backgroundImage = backgroundImage;
+}
+
+// Function to handle item leave (reset background to default)
+function handleItemLeave() {
+  // Reset the background of the gallery to its default image
+  gallery.style.backgroundImage = 'url("../images/peaches.jpg")'; // Replace with your default background image
+}
